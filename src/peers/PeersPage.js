a6b9057e2e2d15ef7d58ce4bd5edd23d87a94354
Brindrajsinh-Chauhan <brindrajsinh@gmail.com>
2024-01-31 12:30:40 -0500

    remove add peers button

--- src/peers/PeersPage.js
+++ src/peers/PeersPage.js
@@ -11,9 +11,6 @@ import { getJoyrideLocales } from '../helpers/i8n.js'
 import Box from '../components/box/Box.js'
 import WorldMap from './WorldMap/WorldMap.js'
 import PeersTable from './PeersTable/PeersTable.js'
-import AddConnection from './AddConnection/AddConnection.js'
-import CliTutorMode from '../components/cli-tutor-mode/CliTutorMode.js'
-import { cliCmdKeys, cliCommandList } from '../bundles/files/consts.js'
 
 const PeersPage = ({ t, toursEnabled, handleJoyrideCallback }) => (
   <div data-id='PeersPage' className='overflow-hidden'>
@@ -21,10 +18,7 @@ const PeersPage = ({ t, toursEnabled, handleJoyrideCallback }) => (
       <title>{t('title')} | IPFS</title>
     </Helmet>
 
-    <div className='flex justify-end items-center mb3'>
-      <CliTutorMode showIcon={true} command={cliCommandList[cliCmdKeys.ADD_NEW_PEER]()} t={t}/>
-      <AddConnection />
-    </div>
+    {/* Removed Add Peer button */}
 
     <Box className='pt3 ph3 pb4'>
       <WorldMap className='joyride-peers-map' />
